ebc5dd45ed1f47017d17b01f64d6d41a6d5c80d6
Suleyman Yildiz <146553553+BM-suleymanyildiz@users.noreply.github.com>
2025-01-19 04:27:16 +0300

    Add files via upload

---
+++ Personel_Kayit/FrmRaporlar.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Personel_Kayit
+{
+    partial class FrmRaporlar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(95, 56);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // FrmRaporlar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 401);
+            this.Controls.Add(this.reportViewer1);
+            this.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "FrmRaporlar";
+            this.Text = "FrmRaporlar";
+            this.Load += new System.EventHandler(this.FrmRaporlar_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ Personel_Kayit/Frmistatistik.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Personel_Kayit
+{
+    partial class Frmistatistik
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lbltoplampersonel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lblevlipersonel = new System.Windows.Forms.Label();
+            this.Lblbekarpersonel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Lblsehirsayisi = new System.Windows.Forms.Label();
+            this.Lbltoplammaas = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.Lblortalamamaas = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toplam Personel Sayısı:";
+            // 
+            // Lbltoplampersonel
+            // 
+            this.Lbltoplampersonel.AutoSize = true;
+            this.Lbltoplampersonel.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lbltoplampersonel.Location = new System.Drawing.Point(270, 23);
+            this.Lbltoplampersonel.Name = "Lbltoplampersonel";
+            this.Lbltoplampersonel.Size = new System.Drawing.Size(23, 23);
+            this.Lbltoplampersonel.TabIndex = 1;
+            this.Lbltoplampersonel.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Evli Personel Sayısı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(229, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bekar Personel Sayısı:";
+            // 
+            // Lblevlipersonel
+            // 
+            this.Lblevlipersonel.AutoSize = true;
+            this.Lblevlipersonel.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lblevlipersonel.Location = new System.Drawing.Point(270, 71);
+            this.Lblevlipersonel.Name = "Lblevlipersonel";
+            this.Lblevlipersonel.Size = new System.Drawing.Size(23, 23);
+            this.Lblevlipersonel.TabIndex = 4;
+            this.Lblevlipersonel.Text = "0";
+            // 
+            // Lblbekarpersonel
+            // 
+            this.Lblbekarpersonel.AutoSize = true;
+            this.Lblbekarpersonel.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lblbekarpersonel.Location = new System.Drawing.Point(270, 114);
+            this.Lblbekarpersonel.Name = "Lblbekarpersonel";
+            this.Lblbekarpersonel.Size = new System.Drawing.Size(23, 23);
+            this.Lblbekarpersonel.TabIndex = 5;
+            this.Lblbekarpersonel.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(124, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Şehir Sayısı:";
+            // 
+            // Lblsehirsayisi
+            // 
+            this.Lblsehirsayisi.AutoSize = true;
+            this.Lblsehirsayisi.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lblsehirsayisi.Location = new System.Drawing.Point(270, 151);
+            this.Lblsehirsayisi.Name = "Lblsehirsayisi";
+            this.Lblsehirsayisi.Size = new System.Drawing.Size(23, 23);
+            this.Lblsehirsayisi.TabIndex = 7;
+            this.Lblsehirsayisi.Text = "0";
+            // 
+            // Lbltoplammaas
+            // 
+            this.Lbltoplammaas.AutoSize = true;
+            this.Lbltoplammaas.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lbltoplammaas.Location = new System.Drawing.Point(270, 187);
+            this.Lbltoplammaas.Name = "Lbltoplammaas";
+            this.Lbltoplammaas.Size = new System.Drawing.Size(23, 23);
+            this.Lbltoplammaas.TabIndex = 9;
+            this.Lbltoplammaas.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(112, 188);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(142, 23);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Toplam Maaş:";
+            // 
+            // Lblortalamamaas
+            // 
+            this.Lblortalamamaas.AutoSize = true;
+            this.Lblortalamamaas.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Lblortalamamaas.Location = new System.Drawing.Point(270, 224);
+            this.Lblortalamamaas.Name = "Lblortalamamaas";
+            this.Lblortalamamaas.Size = new System.Drawing.Size(23, 23);
+            this.Lblortalamamaas.TabIndex = 11;
+            this.Lblortalamamaas.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(95, 225);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(159, 23);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Ortalama Maaş:";
+            // 
+            // Frmistatistik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 333);
+            this.Controls.Add(this.Lblortalamamaas);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.Lbltoplammaas);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.Lblsehirsayisi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Lblbekarpersonel);
+            this.Controls.Add(this.Lblevlipersonel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Lbltoplampersonel);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("DFKaiSho-SB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Frmistatistik";
+            this.Text = "Frmistatistik";
+            this.Load += new System.EventHandler(this.Frmistatistik_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbltoplampersonel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Lblevlipersonel;
+        private System.Windows.Forms.Label Lblbekarpersonel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Lblsehirsayisi;
+        private System.Windows.Forms.Label Lbltoplammaas;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label Lblortalamamaas;
+        private System.Windows.Forms.Label label12;
+    }
+}
